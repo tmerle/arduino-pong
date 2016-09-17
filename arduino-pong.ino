4a220d12a201a9fc87c0a97e270761edb9be4d3b
Bence Darabos <makosbab@gmail.com>
2016-09-17 15:26:47 +0200

    changed rendering to U8G lib

--- arduino-pong.ino
+++ arduino-pong.ino
@@ -6,24 +6,32 @@
   More info about this project can be found on my blog:
   http://michaelteeuw.nl
 
-  Written by Michael Teeuw | Xonay Labs.  
+  Written by Michael Teeuw | Xonay Labs.
   Apache 2 license, all text above must be included 
   in any redistribution.
  ****************************************************/
 
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+/*************************************************** 
+  The game now uses the U8G library, so it can
+  handle my OLED display from ebay, what have some
+  problems with the Adafruit library.
 
-//Define Pins
-#define OLED_RESET 4
+  Partialy re-written by Bence Darabos
+  Apache 2 license, all text above must be included 
+  in any redistribution.
+ ****************************************************/
+
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);  // I2C / TWI 
+
+//Define Pinse
 #define BEEPER 3
 #define CONTROL_A A0
 #define CONTROL_B A1
 
 //Define Visuals
-#define FONT_SIZE 2
+#define FONT_SIZE_W 4
+#define FONT_SIZE_H 10
 #define SCREEN_WIDTH 127  //real size minus 1, because coordinate system starts with 0
 #define SCREEN_HEIGHT 63  //real size minus 1, because coordinate system starts with 0
 #define PADDLE_WIDTH 4
@@ -38,15 +46,14 @@
 
 
 //Define Variables
-Adafruit_SSD1306 display(OLED_RESET);
 
 int paddleLocationA = 0;
 int paddleLocationB = 0;
 
 float ballX = SCREEN_WIDTH/2;
 float ballY = SCREEN_HEIGHT/2;
-float ballSpeedX = 2;
-float ballSpeedY = 1;
+float ballSpeedX = 4;
+float ballSpeedY = 2;
 
 int lastPaddleLocationA = 0;
 int lastPaddleLocationB = 0;
@@ -54,153 +61,154 @@ int lastPaddleLocationB = 0;
 int scoreA = 0;
 int scoreB = 0;
 
+int start = 0;
+int controlAstart;
+int controlBstart;
+
 
 //Setup 
-void setup() 
-{
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
-  display.clearDisplay();   // clears the screen and buffer
-  display.display();   
-  display.setTextWrap(false);
-
-    splash();
-  
-  display.setTextColor(WHITE);
-  display.setTextSize(FONT_SIZE);
-  display.clearDisplay(); 
+void setup(){
+  controlAstart = analogRead(CONTROL_A);
+  controlBstart = analogRead(CONTROL_B);
 }
 
-//Splash Screen
+// //Splash Screen
 void splash()
 {
-  display.clearDisplay(); 
+  centerPrint("PONG",0, 2);
+  centerPrint("By Allan Alcorn",18,1);
+  centerPrint("MichaelTeeuw.nl",27, 1);
+  centerPrint("Bence Darabos",36, 1);
+  centerPrint("Move paddle to start!",SCREEN_HEIGHT-11, 1);
 
-  display.setTextColor(WHITE);
-  centerPrint("PONG",0,3);
-  centerPrint("By Allan Alcorn",24,1);
-  centerPrint("Ported by",33,1);
-  centerPrint("MichaelTeeuw.nl",42,1);
-
-  display.fillRect(0,SCREEN_HEIGHT-10,SCREEN_WIDTH,10,WHITE);
-  display.setTextColor(BLACK);
-  centerPrint("Move paddle to start!",SCREEN_HEIGHT-9,1);
-
-  display.display();
-
-  int controlA = analogRead(CONTROL_A);
-  int controlB = analogRead(CONTROL_B);
-
-  while (abs(controlA - analogRead(CONTROL_A) + controlB - analogRead(CONTROL_B)) < 10) {
-    // show as long as the total absolute change of 
-    // both potmeters is smaler than 5
+  if (abs(controlAstart - analogRead(CONTROL_A) + controlBstart - analogRead(CONTROL_B)) > 50) {
+    //show as long as the total absolute change of 
+    //both potmeters is smaler than 5
+    soundStart();
+    delay(200);
+    start = 1;
   }
-
-  soundStart();
 }
 
 //Loop
 void loop()
 {
   calculateMovement();
-  draw();
+
+  u8g.firstPage();
+  do {
+    draw();
+  u8g.setColorIndex(1);
+  } while( u8g.nextPage() );
 }
 
 void calculateMovement() 
 {
-  int controlA = analogRead(CONTROL_A);
-  int controlB = analogRead(CONTROL_B);
+  if(start == 0){}
+  else{
+    int controlA = analogRead(CONTROL_A);
+    int controlB = analogRead(CONTROL_B);
 
-  paddleLocationA = map(controlA, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
-  paddleLocationB = map(controlB, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
+    paddleLocationA = map(controlA, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
+    paddleLocationB = map(controlB, 0, 1023, 0, SCREEN_HEIGHT - PADDLE_HEIGHT);
 
-  int paddleSpeedA = paddleLocationA - lastPaddleLocationA;
-  int paddleSpeedB = paddleLocationB - lastPaddleLocationB;
+    int paddleSpeedA = paddleLocationA - lastPaddleLocationA;
+    int paddleSpeedB = paddleLocationB - lastPaddleLocationB;
 
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
 
-  //bounce from top and bottom
-  if (ballY >= SCREEN_HEIGHT - BALL_SIZE || ballY <= 0) {
-    ballSpeedY *= -1;
-    soundBounce();
-  }
-
-  //bounce from paddle A
-  if (ballX >= PADDLE_PADDING && ballX <= PADDLE_PADDING+BALL_SIZE && ballSpeedX < 0) {
-    if (ballY > paddleLocationA - BALL_SIZE && ballY < paddleLocationA + PADDLE_HEIGHT) {
+    //bounce from top and bottom
+    if (ballY >= SCREEN_HEIGHT - BALL_SIZE || ballY <= 0) {
+      ballSpeedY *= -1;
       soundBounce();
-      ballSpeedX *= -1;
-    
-      addEffect(paddleSpeedA);
     }
 
+    //bounce from paddle A
+    if (ballX >= PADDLE_PADDING && ballX <= PADDLE_PADDING+BALL_SIZE && ballSpeedX < 0) {
+      if (ballY > paddleLocationA - BALL_SIZE && ballY < paddleLocationA + PADDLE_HEIGHT) {
+        soundBounce();
+        ballSpeedX *= -1;
+      
+        addEffect(paddleSpeedA);
+      }
+    }
+
+    //bounce from paddle B
+    if (ballX >= SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING-BALL_SIZE && ballX <= SCREEN_WIDTH-PADDLE_PADDING-BALL_SIZE && ballSpeedX > 0) {
+      if (ballY > paddleLocationB - BALL_SIZE && ballY < paddleLocationB + PADDLE_HEIGHT) {
+        soundBounce();
+        ballSpeedX *= -1;
+      
+        addEffect(paddleSpeedB);
+      }
+
+    }
+
+    //score points if ball hits wall behind paddle
+    if (ballX >= SCREEN_WIDTH - BALL_SIZE || ballX <= 0) {
+      if (ballSpeedX > 0) {
+        scoreA++;
+        ballX = SCREEN_WIDTH / 4;
+      }
+      if (ballSpeedX < 0) {
+        scoreB++;
+        ballX = SCREEN_WIDTH / 4 * 3;
+      }
+
+      soundPoint();   
+    }
+
+    //set last paddle locations
+    lastPaddleLocationA = paddleLocationA;
+    lastPaddleLocationB = paddleLocationB;  
   }
-
-  //bounce from paddle B
-  if (ballX >= SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING-BALL_SIZE && ballX <= SCREEN_WIDTH-PADDLE_PADDING-BALL_SIZE && ballSpeedX > 0) {
-    if (ballY > paddleLocationB - BALL_SIZE && ballY < paddleLocationB + PADDLE_HEIGHT) {
-      soundBounce();
-      ballSpeedX *= -1;
-    
-      addEffect(paddleSpeedB);
-    }
-
-  }
-
-  //score points if ball hits wall behind paddle
-  if (ballX >= SCREEN_WIDTH - BALL_SIZE || ballX <= 0) {
-    if (ballSpeedX > 0) {
-      scoreA++;
-      ballX = SCREEN_WIDTH / 4;
-    }
-    if (ballSpeedX < 0) {
-      scoreB++;
-      ballX = SCREEN_WIDTH / 4 * 3;
-    }
-
-    soundPoint();   
-  }
-
-  //set last paddle locations
-  lastPaddleLocationA = paddleLocationA;
-  lastPaddleLocationB = paddleLocationB;  
 }
 
 void draw() 
 {
-  display.clearDisplay(); 
-
-  //draw paddle A
-  display.fillRect(PADDLE_PADDING,paddleLocationA,PADDLE_WIDTH,PADDLE_HEIGHT,WHITE);
-
-  //draw paddle B
-  display.fillRect(SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING,paddleLocationB,PADDLE_WIDTH,PADDLE_HEIGHT,WHITE);
-
-  //draw center line
-  for (int i=0; i<SCREEN_HEIGHT; i+=4) {
-    display.drawFastVLine(SCREEN_WIDTH/2, i, 2, WHITE);
+  if(start == 0){
+    splash();
   }
+  else{
+    //draw paddle A
+    u8g.drawVLine(PADDLE_PADDING, paddleLocationA, PADDLE_HEIGHT);
+    u8g.drawVLine(PADDLE_PADDING+1, paddleLocationA, PADDLE_HEIGHT);
 
-  //draw ball
-  display.fillRect(ballX,ballY,BALL_SIZE,BALL_SIZE,WHITE);
+    //draw paddle B
+    u8g.drawVLine(SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING, paddleLocationB, PADDLE_HEIGHT);
+    u8g.drawVLine(SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING+1, paddleLocationB, PADDLE_HEIGHT);
 
-  //print scores
+    //draw center line
+    for (int i=0; i<SCREEN_HEIGHT; i+=4) {
+      u8g.drawVLine(SCREEN_WIDTH/2, i, 2);
+    }
 
-  //backwards indent score A. This is dirty, but it works ... ;)
-  int scoreAWidth = 5 * FONT_SIZE;
-  if (scoreA > 9) scoreAWidth += 6 * FONT_SIZE;
-  if (scoreA > 99) scoreAWidth += 6 * FONT_SIZE;
-  if (scoreA > 999) scoreAWidth += 6 * FONT_SIZE;
-  if (scoreA > 9999) scoreAWidth += 6 * FONT_SIZE;
+    //draw ball
+    for (int i=0; i<BALL_SIZE; i++) {
+      u8g.drawVLine(ballX, ballY, BALL_SIZE);
+      u8g.drawVLine(ballX+i, ballY, BALL_SIZE);
+      u8g.drawVLine(ballX+i, ballY, BALL_SIZE);
+    }
+    //print scores
 
-  display.setCursor(SCREEN_WIDTH/2 - SCORE_PADDING - scoreAWidth,0);
-  display.print(scoreA);
+    //backwards indent score A. This is dirty, but it works ... ;)
+    int scoreAWidth = FONT_SIZE_W;
+    if (scoreA > 9) scoreAWidth += FONT_SIZE_W * 2;
+    if (scoreA > 99) scoreAWidth += FONT_SIZE_W * 2;
+    if (scoreA > 999) scoreAWidth += FONT_SIZE_W * 2;
+    if (scoreA > 9999) scoreAWidth += FONT_SIZE_W * 2;
+    
+    u8g.setFont(u8g_font_unifont);
 
-  display.setCursor(SCREEN_WIDTH/2 + SCORE_PADDING+1,0); //+1 because of dotted line.
-  display.print(scoreB);
-  
-  display.display();
+    u8g.setPrintPos(SCREEN_WIDTH/2 - SCORE_PADDING - scoreAWidth,FONT_SIZE_H+4);
+    u8g.print(scoreA);
+
+    u8g.setPrintPos(SCREEN_WIDTH/2 + SCORE_PADDING+1,FONT_SIZE_H+4);
+    u8g.print(scoreB);
+  }
 } 
+
 
 void addEffect(int paddleSpeed)
 {
@@ -249,10 +257,15 @@ void soundPoint()
   tone(BEEPER, 150, 150);
 }
 
-void centerPrint(char *text, int y, int size)
+void centerPrint(char *text, int y, int font)
 {
-  display.setTextSize(size);
-  display.setCursor(SCREEN_WIDTH/2 - ((strlen(text))*6*size)/2,y);
-  display.print(text);
+  if(font==1){
+    u8g.setFont(u8g_font_6x10);
+    u8g.setPrintPos(SCREEN_WIDTH/2-((strlen(text))*6)/2, y + FONT_SIZE_H);
+  }
+  else{
+    u8g.setFont(u8g_font_unifont);
+    u8g.setPrintPos(SCREEN_WIDTH/2-((strlen(text))*8)/2, y + FONT_SIZE_H);
+  }
+  u8g.print(text);
 }
-
